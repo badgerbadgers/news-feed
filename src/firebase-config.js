ceec5d0ec6dcf0c3827eaad9a510edfe7727a4bf
Tim <tim.almacen@gmail.com>
2022-03-01 18:46:44 -0800

    code cleanup

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -6,18 +6,7 @@ import { getAuth } from "firebase/auth"
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-//   apiKey: `${process.env.REACT_APP_API_KEY}`,
-//   authDomain: `${process.env.REACT_APP_AUTH_DOMAIN}`,
-//   projectId: `${process.env.REACT_APP_PROJECT_ID}`,
-//   storageBucket: `${process.env.REACT_APP_FIREBASE_STORAGE_BUCKET}`,
-//   messagingSenderId: `${process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID}`,
-//   appId: `${process.env.REACT_APP_FIREBASE_APP_ID}`
 apiKey: "AIzaSyAGJwbe89rrQJdfuyiPTrgPuw1tFf7z1mI",
-authDomain: "news-feed-api-dev.firebaseapp.com",
-projectId: "news-feed-api-dev",
-storageBucket: "news-feed-api-dev.appspot.com",
-messagingSenderId: "114504147747",
-appId: "1:114504147747:web:ebd420d1dfbe0a82926296"
 };
 
 // Initialize Firebase
