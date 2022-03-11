d1cade19fce4c15faf4bf6d586fb1381e286a8bf
Tim <39227111+badgerbadgers@users.noreply.github.com>
2022-03-10 22:26:31 -0800

    Update firebase-config.js

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -6,7 +6,7 @@ import { getAuth } from "firebase/auth"
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-apiKey: "AIzaSyAGJwbe89rrQJdfuyiPTrgPuw1tFf7z1mI",
+apiKey: "",
 };
 
 // Initialize Firebase
